4494d2d0dac36ccf9e844de80d4941e74e2a6acb
srinivasminigula <srinivasminigula@192.168.1.220>
2019-07-29 22:55:41 +0530

    Marking class as final

--- src/main/java/com/virtusa/galaxy/merchant/constants/Constants.java
+++ src/main/java/com/virtusa/galaxy/merchant/constants/Constants.java
@@ -7,7 +7,7 @@ package com.virtusa.galaxy.merchant.constants;
  * @author srinivasminigula
  *
  */
-public class Constants {
+public final class Constants {
 	private Constants() {
 	}
 	public static final int LEGAL_REPETITION_LIMIT = 3;
